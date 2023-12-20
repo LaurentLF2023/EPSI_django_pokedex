04ea2ec43cba9c46d710f8ccf727f0c8d6624258
blambertt <bastien.lambert2022@campus-eni.fr>
2023-12-20 13:39:07 +0100

    start dev

---
+++ epsi_pokedex/pokeapi/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+import requests
+import json
+from django.template import loader
+
+URL_BASE = 'http://pokeapi.co/api/v1/pokemon/'
+
+
+def index(request):
+    response = requests.get(URL_BASE)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        template = loader.get_template("pokeapi/index.html")
+        context = {
+            "data": data,
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        return HttpResponse("")
+
+
+def detail(request, pokemon_id):
+    response = requests.get(URL_BASE + "/" + str(pokemon_id))
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        template = loader.get_template("pokeapi/detail.html")
+        context = {
+            "data": data,
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        return HttpResponse("")
